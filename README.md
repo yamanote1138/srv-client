4078899d7155b8bb54b7c225062f30d4c5bb2926
Chad Francis <discgolfer1138@gmail.com>
2013-04-25 00:32:10 -0600

    starting to take shape, began basic cli test

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ node-surveyor
 =============
 
 node wrapper for TCP communication with Surveyor SRV-1 robot
+
+motor commands are specified here
+
+	http://www.surveyor.com/SRV_protocol.html
--- tests/test.js
+++ tests/test.js
@@ -1,33 +1,53 @@
 var srv = require('../lib/srv.js'),
-	prompt = require('prompt'),
-	assert = require('chai').assert;
+	prompt = require('prompt');
 
-describe('Srv1Client', function(done) {
 	
-	var srvClient;
+var srvClient;
 
-	before(function(done){
-		prompt.start();
+prompt.start();
 
-		prompt.get(['host'], function (err, result) {
-			if (err) { console.log(err); return; }
+prompt.get(['host'], function (err, result) {
+	if (err) { console.log(err); return; }
 			
-			srvClient = new srv({
-				host: result.host
-			});
-			done();
-		});
+	srvClient = new srv({
+		host: result.host
 	});
 
-	describe('#laserOn()', function(done) {
-		it('should turn laser on and return ack', function(done) {
-			srvClient.laserOn(function(err, ack){
-				assert.isNull(err, 'err is not null');
-				assert.isNotNull(data, 'ack is null');
-				console.log(ack);
-				done();
-			});
-		});
+	srvClient.connect(function(){
+		awaitCmd();
 	});
 
 });
+
+function handleCmd(cmd){
+	switch(cmd){
+		case 'l':
+			srvClient.laserOn();
+			awaitCmd();
+			break
+		case 'L':
+			srvClient.laserOff();
+			awaitCmd();
+			break
+		case 'd':
+		case 'D':
+			srvClient.disconnect();
+			process.exit(code=0);
+			break
+		default:
+			console.log('unknown cmd');
+			awaitCmd();
+			break
+	}
+}
+
+function awaitCmd(){
+	prompt.get(['cmd'], function (err, result){
+		if (err) {
+			console.log(err);
+			srvClient.disconnect();
+			process.exit(1);
+		}
+		handleCmd(result.cmd);
+	})
+}
